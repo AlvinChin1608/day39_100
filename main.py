b484167ad24a161732b8fcd1df29258dbeba6e16
Alvin Chin <32458011+AlvinChin1608@users.noreply.github.com>
2024-06-20 20:03:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -61,13 +61,6 @@ for destination in sheet_data:
 
     if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
         print(f"Lower price flight found to {destination['city']}!")
-        # notification_manager.send_sms(
-        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
-        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        # )
-
-        # SMS not working? Try whatsapp instead.
         notification_manager.send_whatsapp(
             message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
                          f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
